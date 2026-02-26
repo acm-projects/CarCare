576c9865fdd3f097f273aad8d14e033a5fd14616
Maddy Quinn <quinn.mbv@gmail.com>
2026-02-26 16:25:00 -0600

    global style sheet created

---
+++ frontend/CarCare/styles/global.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create ({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center',
+        alignItems: 'center',
+        gap: 15,
+        backgroundColor: '#386FA4',
+    },
+
+    whiteTitle: {
+        fontFamily: 'Onest',
+        fontSize: 50,
+        color: '#fff'
+    },
+
+    gradientTitle: {
+        fontFamily: 'Onest',
+        fontSize: 50,
+    },
+    
+    gradientHeader: {
+        fontFamily: 'Onest',
+    },
+
+    
+});
