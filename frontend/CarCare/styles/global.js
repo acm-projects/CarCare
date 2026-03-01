a95da9685dbe48ebdddc1613e7ace9dccf71d00a
Maddy Quinn <quinn.mbv@gmail.com>
2026-03-01 09:49:40 -0600

    navigation page to page to enter VIN. gradient title added to global styles

--- frontend/CarCare/styles/global.js
+++ frontend/CarCare/styles/global.js
@@ -1,8 +1,50 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import MaskedView from '@react-native-masked-view/masked-view';
+
+export function GradientText({ children }) {
+  return (
+    <MaskedView
+      style={{ alignSelf: 'flex-start' }}
+      maskElement={
+        <View>
+          <Text style={globalStyles.gradientTitle}>
+            {children}
+          </Text>
+        </View>
+      }
+    >
+      <LinearGradient
+        colors={['#386FA4', '#84D2F6']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+      >
+        <Text
+          style={[
+            globalStyles.gradientTitle,
+            { opacity: 0 }
+          ]}
+        >
+          {children}
+        </Text>
+      </LinearGradient>
+    </MaskedView>
+  );
+}
 
 export const globalStyles = StyleSheet.create ({
     container: {
         flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center',
+        alignItems: 'center',
+        gap: 15,
+    },
+
+    containerWhite: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
         justifyContent: 'center',
         textAlign: 'center',
         alignItems: 'center',
@@ -24,5 +66,4 @@ export const globalStyles = StyleSheet.create ({
         fontFamily: 'Onest',
     },
 
-    
 });
