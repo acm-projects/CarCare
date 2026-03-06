be470d2d2bac0433b94fd864d115b8974ede536d
Maddy Quinn <quinn.mbv@gmail.com>
2026-03-06 16:44:56 -0600

    vinEnter page started

--- frontend/CarCare/app/vinEnter.tsx
+++ frontend/CarCare/app/vinEnter.tsx
@@ -1,20 +1,94 @@
 import { Image } from 'expo-image';
-import { Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, TextInput, Alert, Button, View, TouchableOpacity, useWindowDimensions } from 'react-native';
+import ParallaxScrollView from '@/components/parallax-scroll-view';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
 import { Link, useRouter } from 'expo-router';
 import { globalStyles, GradientText } from '../styles/global';
 import { LinearGradient } from 'expo-linear-gradient';
+import MaskedView from '@react-native-masked-view/masked-view';
+import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 
-export default function vinEnter(){
+export default function HomeScreen() {
+  
+  const { height } = useWindowDimensions();
+  const router = useRouter();
+  const navigate = () => {
+    router.push("/vinEnter")
+  };
+  
+  const handlePress = () => {
+    Alert.alert('CarCare Log In', 'You have logged in successfully!');
+  };
 
-    const router = useRouter();
-    const handlePress = () => {
-        router.push("/logIn")
-    };
-
-
-    return (
-        <View style = {globalStyles.containerWhite}>
-            <GradientText style={{ fontSize: 25 }}>Vin Enter</GradientText>
+  return (
+      <LinearGradient
+    colors={['#386FA4', '#84D2F6']}
+    start={{ x: 1, y: 0.5 }}
+    end={{ x: 0, y: 0.5 }}
+    style={{ flex: 1 }}
+  >
+    <View style={globalStyles.container}>
+      <View style =  {{position: 'absolute', top: 100, alignItems: 'center'}}>
+        <Text style = {globalStyles.whiteTitle}>Create your car profile</Text>
+        <Image source = {require('../assets/images/CarCareLogoGearWhite.png')}
+          style={{width: 250, height:250, top: 150, position: 'absolute'}}></Image>
+      </View>
+      <View style = {[styles.logInContainer, { height: .55 * height}]}>
+        <View style = {styles.subContainer}>
+          <GradientText style={globalStyles.gradientH2}>VIN Number</GradientText>
+          <TextInput
+            style={styles.logInBox}
+            placeholder="Enter VIN number"
+            placeholderTextColor={'#8d8d8d'}
+            />
+            <Text style = {globalStyles.grayP}>CarCare needs your car’s VIN number to access accurate technical specifications, 
+                maintenance records, and manufacturing data.</Text>
         </View>
-    )
+        <View>
+            <TouchableOpacity style={globalStyles.whiteButton} onPress={() => router.push('/')}>
+                <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#84D2F6', '#386FA4']} style={globalStyles.gradientButton}>
+                    <Text style={globalStyles.whiteButtonText}>
+                    Log In
+                    </Text>
+                </LinearGradient>
+            </TouchableOpacity>
+            <TouchableOpacity style={globalStyles.whiteButton} onPress={() => router.push('/logIn')}>
+                <Text style = {[globalStyles.grayH2, {textAlign: 'center'}]}>Back</Text>
+            </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  </LinearGradient>
+  );
 }
+
+const styles = StyleSheet.create({
+  
+  subContainer: {
+    width: 300,
+    gap: 20,
+  },
+
+  logInContainer:{
+    flex: 1,
+    backgroundColor: '#fff',
+    position: 'absolute',
+    bottom: 0,
+    height: 50,
+    borderRadius: 50,
+    width: 415,
+    padding: 35,
+    gap: 125,
+    alignItems: 'center',
+  },
+
+  logInBox: {
+    borderColor: 'transparent',
+    borderWidth: 0.75,
+    borderBottomColor: '#8d8d8d',
+    width: 300,
+    paddingBottom: 5
+  },
+
+});
