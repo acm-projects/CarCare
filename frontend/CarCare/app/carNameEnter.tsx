bd8ba3b54e4fb9d5e8764b49b9f2bf7a212072a4
Maddy Quinn <quinn.mbv@gmail.com>
2026-03-30 21:03:53 -0500

    gradient darkened on vinEnter and carNameEnter

--- frontend/CarCare/app/carNameEnter.tsx
+++ frontend/CarCare/app/carNameEnter.tsx
@@ -22,7 +22,7 @@ export default function carNameEnter() {
 
   return (
     <LinearGradient
-      colors={['#386FA4', '#84D2F6']}
+      colors={['#386FA4', '#53c1f3']}
       start={{ x: 1, y: 0.5 }}
       end={{ x: 0, y: 0.5 }}
       style={{ flex: 1 }}>
@@ -64,7 +64,7 @@ export default function carNameEnter() {
           </View>
           <View>
             <TouchableOpacity style={globalStyles.whiteButton} onPress={() => router.push('../myGarage')}>
-              <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#84D2F6', '#386FA4']} style={globalStyles.gradientButton}>
+              <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#53c1f3', '#386FA4']} style={globalStyles.gradientButton}>
                 <Text style={globalStyles.whiteButtonText}>
                   Done
                 </Text>
--- frontend/CarCare/app/vinEnter.tsx
+++ frontend/CarCare/app/vinEnter.tsx
@@ -22,7 +22,7 @@ export default function vinEnter() {
 
   return (
     <LinearGradient
-      colors={['#386FA4', '#84D2F6']}
+      colors={['#3272ae', '#53c1f3']}
       start={{ x: 1, y: 0.5 }}
       end={{ x: 0, y: 0.5 }}
       style={{ flex: 1 }}>
@@ -65,7 +65,7 @@ export default function vinEnter() {
           </View>
           <View>
             <TouchableOpacity style={globalStyles.whiteButton} onPress={() => router.push('../carNameEnter')}>
-              <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#84D2F6', '#386FA4']} style={globalStyles.gradientButton}>
+              <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#53c1f3', '#3272ae']} style={globalStyles.gradientButton}>
                 <Text style={globalStyles.whiteButtonText}>
                   Next
                 </Text>
